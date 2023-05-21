ec8a03aeeb7184c4cbd91fec7d80de54ee3c142b
DataWizard-Yash <yashbhatt.datascience@gmail.com>
2023-05-22 00:32:44 +0530

    Initial Commit with working Demo

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+# Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="Yash Bhatt", page_icon=":tada:", layout="wide")
+
+# ------- HEADER SECTION -------
+st.subheader("Hello, I am Yash Bhatt :wave:")
+st.title(" A Data Science and Analytics Professional and Python Developer")
+
